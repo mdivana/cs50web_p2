588a9c253f4a17c08d30068fc7f1b308bf1dde17
Nika Mdivani <nikamdivani@icloud.com>
2022-05-13 15:44:00 +0400

    first commit

---
+++ auctions/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+class User(AbstractUser):
+    pass
